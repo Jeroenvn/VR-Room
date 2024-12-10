43f3f568efc4f687634cb3105e8ec736ed2f6056
Jeroen <s1137642@student.hsleiden.nl>
2024-12-10 16:10:49 +0100

    feat: added card with method that can only be called when held

---
+++ Assets/My Assets/Scripts/ThrowableCard.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableCard : MonoBehaviour
+{
+    public void Pickup(){
+
+    }
+
+    public void Throw(){
+        Debug.Log("Throw called!");
+    }
+
+}
